8ab51c8399cdae7ba9b18aa693999a9d8141ce69
Thomas Burke <etburke4@gmail.com>
2012-10-31 09:33:08 -0700

    added classes to table and actions

--- lib/active_admin/views/index_as_table.rb
+++ lib/active_admin/views/index_as_table.rb
@@ -101,7 +101,7 @@ module ActiveAdmin
         table_options = {
           :id => active_admin_config.resource_name.plural,
           :sortable => true,
-          :class => "index_table index",
+          :class => "index_table index table table-condensed",
           :i18n => active_admin_config.resource_class,
           :paginator => page_presenter[:paginator] != false
         }
@@ -153,13 +153,13 @@ module ActiveAdmin
           column options[:name] do |resource|
             links = ''.html_safe
             if controller.action_methods.include?('show')
-              links << link_to(I18n.t('active_admin.view'), resource_path(resource), :class => "member_link view_link")
+              links << link_to(I18n.t('active_admin.view'), resource_path(resource), :class => "member_link view_link btn btn-mini")
             end
             if controller.action_methods.include?('edit')
-              links << link_to(I18n.t('active_admin.edit'), edit_resource_path(resource), :class => "member_link edit_link")
+              links << link_to(I18n.t('active_admin.edit'), edit_resource_path(resource), :class => "member_link edit_link btn btn-mini")
             end
             if controller.action_methods.include?('destroy')
-              links << link_to(I18n.t('active_admin.delete'), resource_path(resource), :method => :delete, :data => {:confirm => I18n.t('active_admin.delete_confirmation')}, :class => "member_link delete_link")
+              links << link_to(I18n.t('active_admin.delete'), resource_path(resource), :method => :delete, :data => {:confirm => I18n.t('active_admin.delete_confirmation')}, :class => "member_link delete_link btn btn-mini btn-danger")
             end
             links
           end
